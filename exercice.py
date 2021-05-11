f217c632d315aede27c969a43b1dbc591a966a56
Thravior <42951795+Thravior@users.noreply.github.com>
2021-05-11 15:08:19 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,7 +2,16 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
-    return mot
+    temp=[]
+    for lettre in mot:
+        if ord(lettre) == 32:
+            temp.append(lettre)
+        else:
+            temp.append(chr(ord(lettre)-32))
+    resultat = ''.join(temp)
+    return resultat
+
+### cette fonction échoue si des charactères autres que des lettres son envoyé
 
 
 if __name__ == '__main__':
